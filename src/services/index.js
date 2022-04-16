55117cd719a499f170b16c469863386261191750
RupakSapkota123 <rupaksapkota2000@gmail.com>
2022-04-16 19:15:42 +0545

    initilized customerService to index.js

--- src/services/index.js
+++ src/services/index.js
@@ -6,3 +6,4 @@ export { default as authService } from './Auth/auth.service.js';
 export { default as brandService } from './Items/brand.service.js';
 export { default as productsService } from './Items/products.service.js';
 export { default as categoriesService } from './Items/categories.service.js';
+export { default as customerService } from './sales/customer.service.js';
